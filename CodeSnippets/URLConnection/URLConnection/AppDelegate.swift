c0e8862f9b2791cb40612ce45b13cdbcaddd171a
Peter Stöhr <peter.stoehr@hof-university.de>
2016-10-11 15:06:00 +0200

    Einfache macOS Demo für HTTPS mit Login
    Swift 3 Version für einen HTTP-Request mit Login

---
+++ CodeSnippets/URLConnection/URLConnection/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  URLConnection
+//
+//  Created by Peter Stöhr on 11.10.16.
+//  Copyright © 2016 Peter Stöhr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
