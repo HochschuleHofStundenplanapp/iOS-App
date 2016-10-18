23da2371930af8bffc2ecf64239af67ea4fefd67
Jonas <beetz.jonas@t-online.de>
2016-10-18 11:15:22 +0200

    Zweiter Entwurf der Navigationsstruktur
    - Die App wurde in eine Tabbed Application umgewandelt.
    - Reine UI - Keine Logik

---
+++ CodeSnippets/StundenplanNavigation/StundenplanNavigation/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  StundenplanNavigation
+//
+//  Created by Jonas Beetz on 18.10.16.
+//  Copyright © 2016 Jonas Beetz. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
