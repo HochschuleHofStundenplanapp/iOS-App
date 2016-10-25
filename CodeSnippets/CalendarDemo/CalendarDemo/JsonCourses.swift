c2ffa2e8e88da76216ad215e2f49d49759802470
fuhrmann2 <sfuhrmann@hof-university.de>
2016-10-25 08:06:43 +0200

    JsonCourses
    Hinzugefügt

---
+++ CodeSnippets/CalendarDemo/CalendarDemo/JsonCourses.swift
@@ -0,0 +1,58 @@
+//
+//  JsonRecommendation.swift
+//  SechQueryComposer
+//
+//  Copyright © 2015 Peter Stoehr. All rights reserved.
+//
+
+import Foundation
+
+class JsonCourses {
+    fileprivate var pCourses : Courses?
+    
+    var courses : Courses? {
+        get {
+            return pCourses
+        }
+    }
+    
+    init? (data : Data)
+    {
+        let jsonT = try? JSONSerialization.jsonObject(with: data, options: []) as AnyObject
+        guard let json = jsonT else {
+            return nil
+        }
+        extractCourses(json)
+    }
+    
+    fileprivate func extractCourses(_ json : AnyObject)
+    {
+        guard let jsonData = JSONData.fromObject(json) else
+        {
+            return
+        }
+        guard let allResults = jsonData["courses"]?.array as [JSONData]! else
+        {
+            return
+        }
+        
+        pCourses = Courses()
+        
+        for i in allResults
+        {
+            let con = (i["course"]?.string)!
+            let nameD = (i["labels"]?["de"]?.string)!
+            let nameE = (i["labels"]?["en"]?.string)!
+            let sem = (i["semester"]?.array)!
+            
+            var newSem : [String] = []
+            
+            for s in sem{
+                newSem.append(s.string!)
+            }
+
+            let newCourse = Course(contraction : con, nameDe: nameD, nameEn: nameE, semester: newSem)
+            pCourses?.courses.append(newCourse)
+        }
+    }
+}
