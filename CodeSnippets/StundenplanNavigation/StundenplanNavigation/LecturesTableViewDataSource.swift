370d19309bbdd12d7fafd643977bedc28fa8bed3
Peter Stöhr <peter.stoehr@hof-university.de>
2017-01-18 07:56:26 +0100

    Verschiedene Vorlesungsgruppen sind jetzt im Settingsscreen unterscheidbar

--- CodeSnippets/StundenplanNavigation/StundenplanNavigation/LecturesTableViewDataSource.swift
+++ CodeSnippets/StundenplanNavigation/StundenplanNavigation/LecturesTableViewDataSource.swift
@@ -11,10 +11,32 @@ import UIKit
 class LecturesTableViewDataSource: NSObject, UITableViewDataSource {
 
     var networkController : NetworkController
+    var lastLectureGroup : (courseInfo : String, semInfo : String)
+    var hightLightLine : Bool
     
     init(tableView : LecturesTableViewController) {
         networkController = NetworkController()
         networkController.loadSchedule(tableView: tableView)
+        lastLectureGroup.courseInfo = ""
+        lastLectureGroup.semInfo = ""
+        hightLightLine = false
+    }
+    
+    private func computeBackgroundColor (currentCourse : String, currentSemester : String) -> UIColor
+    {
+        if currentCourse != lastLectureGroup.courseInfo || currentSemester != lastLectureGroup.semInfo
+        {
+            hightLightLine = !hightLightLine
+            lastLectureGroup.courseInfo = currentCourse
+            lastLectureGroup.semInfo = currentSemester
+        }
+        
+        if hightLightLine
+        {
+            return UIColor.white
+        }
+        
+        return UIColor(red: 0.925, green: 0.925, blue: 0.925, alpha: 1.0)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
@@ -22,7 +44,11 @@ class LecturesTableViewDataSource: NSObject, UITableViewDataSource {
         
         let lecture = Settings.sharedInstance.tmpSchedule.getLectureAt(section: indexPath.section, row: indexPath.row)
         
+        let course = lecture.course.nameDe
+        let semester = lecture.semester.name
+        let cellColor = computeBackgroundColor(currentCourse: course, currentSemester: semester)
         
+        cell.backgroundColor = cellColor
         cell.courseLabel.text = lecture.name
         cell.docentLabel.text = lecture.lecturer
         cell.commentLabel.text = lecture.comment
