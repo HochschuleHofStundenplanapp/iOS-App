99877d50ef5c98b401754bfd811c11be20aea617
Daniel Glaser <daniel.glaser@hof-university.de>
2017-05-30 10:16:23 +0200

    CalendarInterface eingebaut. Funktioniert so noch nicht.

---
+++ CodeSnippets/StundenplanNavigation/StundenplanNavigation/CalendarData.swift
@@ -0,0 +1,33 @@
+//
+//  CalendarEventIds.swift
+//  StundenplanNavigation
+//
+//  Created by Daniel on 27.04.17.
+//  Copyright © 2017 Jonas Beetz. All rights reserved.
+//
+
+import Foundation
+
+class CalendarData: NSObject, NSCoding {
+    static var sharedInstance = CalendarData()
+    
+    var lecturesEventIdDictonary : [Int:[String]] = [:]
+    var changesEventIdDictonary : [Int:[String]] = [:]
+    let lecturesEventIdDictonaryKey = "lecturesEventIdDictonary"
+    let changesEventIdDictonaryKey = "changesEventIdDictonary"
+    
+    private override init () {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        lecturesEventIdDictonary = aDecoder.decodeObject(forKey: lecturesEventIdDictonaryKey) as! Dictionary
+        changesEventIdDictonary = aDecoder.decodeObject(forKey: changesEventIdDictonaryKey) as! Dictionary
+        super.init()
+    }
+    
+    func encode(with aCoder: NSCoder){
+        aCoder.encode(lecturesEventIdDictonary, forKey: lecturesEventIdDictonaryKey)
+        aCoder.encode(changesEventIdDictonary, forKey: changesEventIdDictonaryKey)
+    }
+}
