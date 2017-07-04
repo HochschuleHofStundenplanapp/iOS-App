0859838339fbf21b4e7b4de630a03c0d7a608691
Daniel Zizer <danielzizer@wstud2-a0999b1c53f9.hof-university.de>
2017-07-04 12:27:16 +0200

    AllChanges in UserData Zugriffsschichten verschoben

--- CodeSnippets/StundenplanNavigation/StundenplanNavigation/AllChanges.swift
+++ CodeSnippets/StundenplanNavigation/StundenplanNavigation/AllChanges.swift
@@ -10,33 +10,33 @@ import UIKit
 
 class AllChanges: NSObject {
 
-    //fileprivate var serverData = ServerData.sharedInstance
+    fileprivate var userData = UserData.sharedInstance
     
     func numberOfEntries(for section : Int) -> Int
     {
-        return UserData.sharedInstance.oldChanges.count
+        return userData.oldChanges.count
     }
     
     func getElement(at index : Int) -> ChangedLecture
     {
-        return UserData.sharedInstance.oldChanges[index]
+        return userData.oldChanges[index]
     }
     
     func getChangedLectures() -> [ChangedLecture] {
-        return UserData.sharedInstance.oldChanges
+        return userData.oldChanges
     }
     
     func append(chLectures : [ChangedLecture]) {
         
         for lec in chLectures{
             //Check chLecture duplicates
-            if !UserData.sharedInstance.oldChanges.contains(lec){
-                UserData.sharedInstance.oldChanges.append(lec)
+            if !userData.oldChanges.contains(lec){
+                userData.oldChanges.append(lec)
             }
         }
     }
     
     func clear() {
-        UserData.sharedInstance.oldChanges.removeAll()
+        userData.oldChanges.removeAll()
     }
 }
