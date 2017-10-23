7c7bdcfd395b6790bc590d48a41c85ded36ca163
Normen <normen.krug@web.de>
2017-10-23 12:07:51 +0200

    Added Device Token request

--- StundenplanNavigation/StundenplanNavigation/AppDelegate.swift
+++ StundenplanNavigation/StundenplanNavigation/AppDelegate.swift
@@ -29,9 +29,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate, myObserverProtocol {
         
 //        print("Server: \(ServerData.sharedInstance.allChanges)")
         
+        //
+        UIApplication.shared.registerForRemoteNotifications()
+
         return true
     }
 
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("DeviceToken1: \(deviceToken)")
+        let token = deviceToken.map { String(format: "%02.2hhx", $0) }.joined()
+        print("DeviceToken: \(token)")
+    }
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("error APNS")
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
