23599f4106ed794571eaf2b2eada748fb6e40c46
Christian Pfeiffer <cpfeiffer@hof-university.de>
2017-11-06 19:30:50 +0100

    Fixed Crash for EmptyScheduleTableViewCell * Fixed Storyboard Warings

--- StundenplanNavigation/StundenplanNavigation/ScheduleTableViewDelegate.swift
+++ StundenplanNavigation/StundenplanNavigation/ScheduleTableViewDelegate.swift
@@ -24,56 +24,40 @@ class ScheduleTableViewDelegate: NSObject, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
- //       if !cell.isExpanded {
-  //          cell.isExpanded = true
-  //      }
-//        else{
- //           cell.isExpanded = false
-  //x      }
-        
         if indexPath != selectedIndexPath{
             selectedIndexPath = indexPath
         } else {
             selectedIndexPath = nil
         }
         tableView.reloadRows(at: [indexPath], with: .automatic)
-
-          //rotate Aufklapp-Pfeil
-        let cell : ScheduleTableViewCell = tableView.cellForRow(at: indexPath) as! ScheduleTableViewCell!
         
-        if(indexPath == selectedIndexPath) {
-           cell.OpenButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
-            print("Cell rotation for \(indexPath)")
-        } else {
-            cell.OpenButton.transform = CGAffineTransform(rotationAngle: 0.0)
+        if let cell : ScheduleTableViewCell = tableView.cellForRow(at: indexPath) as? ScheduleTableViewCell {
+            if(indexPath == selectedIndexPath) {
+                cell.OpenButton.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                cell.setExpandedState(newState: true)
+            } else {
+                cell.OpenButton.transform = CGAffineTransform(rotationAngle: 0.0)
+                cell.setExpandedState(newState: false)
+            }
         }
     }
         
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        var newHeigth : CGFloat = 58
         
         if SelectedLectures().getOneDimensionalList().count > 0 {
             let lecture = SelectedLectures().getElement(at: indexPath)
             
             if(selectedIndexPath != nil){
                 if indexPath == selectedIndexPath{
-                    if(lecture.comment == ""){
-                        return 92
-                    }
-                    else{
-                        return 107
-                    }
+                    if(lecture.comment == ""){newHeigth = 92}
+                    else{newHeigth = 107}
                 }
-                else{
-                    return 58
-                
-                }
+                else {newHeigth = 58}
             }
-            else{
-                return 58
-            }
+            else{newHeigth = 58}
 
-        } else {
-            return CGFloat(107)
-        }
+        } else {newHeigth = 107}
+        return newHeigth
     }
 }
